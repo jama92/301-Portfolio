f7e9435fef468badd31ba3d165315a011156608d
jama92 <andrewmceniry@gmail.com>
2016-01-11 00:11:05 -0800

    Assignment 1

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,40 @@
 
 
-var Project = function(project) {
+var projects = [];
 
-  this.title = project.title;
-  this.date = project.date;
-  this.projectUrl = project.projectUrl;
+function Project (prop) {
 
+  this.title = prop.title;
+  this.projectUrl = prop.authorUrl;
+  this.date = prop.date;
+}
+
+Project.prototype.toHtml = function() {
+
+  var $newArticle = $('article.template').clone();
+
+  $newArticle.removeClass('template');
+  $newArticle.addClass('draft');
+
+
+  $newArticle.find('.projectTitle').html(this.title);
+  $newArticle.find('.projectUrl').attr('href', this.projectUrl);
+
+  $newArticle.find('.projectDate').attr('datetime', this.date);
+  $newArticle.find('.projectDate').attr('title', this.date);
+  $newArticle.find('.projectDate').html('about ' + parseInt((new Date() - new Date(this.date))/60/60/24/1000) + ' days ago');
+
+  return $newArticle;
 };
 
-Project.prototype.toHTML = function() {
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
 
-  var $newProject = $('projects.template').clone();
+rawData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
 
-
-  $newProject.find('projectTitle').html(this.title);
-  $newProject.find('projectDate').html(this.date);
-  $newProject.find('projectLink').attr('href', this.projectUrl);
-
-};
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
